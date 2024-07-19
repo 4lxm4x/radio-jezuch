ffe6d838704f3fb1987efb21f16e4fd14b5601a9
Oleksandr Maksymenko <machinery69@gmail.com>
2024-07-19 17:07:20 +0300

    edit placeholder

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -26,8 +26,23 @@ export default function Info() {
       const { data } = await axios(
         'https://myradio24.com//users/jezuch/status.json'
       );
+      const dataFromLastFM = await axios(
+        `https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key= f4d93b703b4b02e3b4040a313bf98512&artist=${data.artist}&track=${data.songtitle}&format=json`
+      );
+      console.log(dataFromLastFM);
+      // let images = {};
+      // dataFromLastFM.data.track.album.image.reduce()
 
-      return data;
+      const fullData = {
+        ...data,
+        normalImg: dataFromLastFM.data.track
+          ? dataFromLastFM.data.track.album.image[2]['#text']
+          : require('../../images/placeholder.png'),
+      };
+
+      // dataFromLastFM.data.track.album.image[2]['#text'],
+      console.log(fullData);
+      return fullData;
     } catch (error) {
       console.log(error);
     }
@@ -35,6 +50,7 @@ export default function Info() {
 
   return (
     <div>
+      <img src={streamData.normalImg} alt={streamData.songtitle} />
       <h2>{streamData.artist}</h2>
       <h3>{streamData.songtitle}</h3>
     </div>
