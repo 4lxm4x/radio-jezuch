186d751259e74c84b78b3bb53116501c5d2dcfb7
Oleksandr Maksymenko <machinery69@gmail.com>
2024-07-21 16:47:33 +0300

    edited fetching

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -12,7 +12,16 @@ export default function Info() {
     const interval = setInterval(async function updateInfo() {
       const data = await getData();
       if (data.songtitle !== streamData.songtitle) {
-        setStreamData(data);
+        const imageUrl = await getCover(data.artist, data.songtitle);
+        const fullData = {
+          ...data,
+          normalImg:
+            imageUrl === ''
+              ? require('../../images/placeholder.png')
+              : imageUrl,
+        };
+
+        setStreamData(fullData);
       }
     }, 5000);
 
@@ -26,31 +35,34 @@ export default function Info() {
       const { data } = await axios(
         'https://myradio24.com//users/jezuch/status.json'
       );
-      const dataFromLastFM = await axios(
-        `https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key= f4d93b703b4b02e3b4040a313bf98512&artist=${data.artist}&track=${data.songtitle}&format=json`
-      );
-      console.log(dataFromLastFM);
-      // let images = {};
-      // dataFromLastFM.data.track.album.image.reduce()
-
-      const fullData = {
-        ...data,
-        normalImg: dataFromLastFM.data.track
-          ? dataFromLastFM.data.track.album.image[2]['#text']
-          : require('../../images/placeholder.png'),
-      };
 
       // dataFromLastFM.data.track.album.image[2]['#text'],
-      console.log(fullData);
-      return fullData;
+      //   console.log(fullData);
+      return data;
     } catch (error) {
       console.log(error);
     }
   }
 
+  async function getCover(artist, songtitle) {
+    const dataFromLastFM = await axios(
+      `https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key= f4d93b703b4b02e3b4040a313bf98512&artist=${artist}&track=${songtitle}&format=json`
+    );
+
+    const imageUrl =
+      dataFromLastFM.data?.track?.album?.image[2]['#text'] ??
+      require('../../images/placeholder.png');
+
+    return imageUrl;
+  }
+
   return (
     <div>
-      <img src={streamData.normalImg} alt={streamData.songtitle} />
+      <img
+        src={streamData.normalImg}
+        alt={streamData.songtitle}
+        // width={'174px'}
+      />
       <h2>{streamData.artist}</h2>
       <h3>{streamData.songtitle}</h3>
     </div>
